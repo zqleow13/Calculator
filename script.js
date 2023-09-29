3eab6823f5787ecb78438521837cb3cb6c9c149c
zqleow13 <hayleyleow13@gmail.com>
2023-09-29 10:54:09 +0800

    Completed the grid style for the keys in css

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 const buttonKeys = document.querySelectorAll('button');
-let keys = 1;
+
 
 // making the buttons click when pressed
-buttonKeys.forEach((element) => {
-    element.addEventListener('click', function() {
-        keypress(element.attributes["data-key"].value)
-    });
-});
-
-
-function keypress(key) {
-    if (key === '=') {
-        //enterKey();
-        } else if (key === 'AC') {
-            //deleteKey();
-        } else {
-            //populateKey(key);
-        }
-}
