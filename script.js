faa96cc2d04d1eb7a4b0757c5a828708135d8ddb
zqleow13 <hayleyleow13@gmail.com>
2023-09-30 22:07:26 +0800

    Added comments in js

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-const buttonKeys = document.querySelectorAll('button');
+const numberButtons = document.querySelectorAll('[data-key]');
+const operationButtons = document.querySelectorAll('[data-action]');
 
+// to clear numbers in display
+clear() {
 
-// making the buttons click when pressed
+}
+
+// to delete numbers in display
+delete() {
+
+}
+
+// to show numbers in display
+appendNumber(number) {
+
+}
+
+// to activate operations when clicked
+chooseOperation(operation) {
+
+}
+
+// to compute the input
+compute() {
+
+}
+
+// to update display
+updateDisplay() {
+    
+}
