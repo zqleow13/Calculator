9746eda932889b5a39b702574fe604097af32154
zqleow13 <hayleyleow13@gmail.com>
2023-10-03 14:59:11 +0800

    To display numbers when the buttons are clicked

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
-const numberButtons = document.querySelectorAll('[data-key]');
-const operationButtons = document.querySelectorAll('[data-action]');
-let displayOutput = document.querySelector('.display');
-let display = 0;
-
-// to clear numbers in display
-function clear() {
-operationButtons.forEach(element) {
-    if (operationButtons.getAttribute('data-action') === 'clear') {
-        //updateDisplay('0');
-    } 
-}
-}
-
-// to delete numbers in display
-function delete() {
-
-}
-
-// to show numbers in display
-function appendNumber(number) {
-    for (let i = 0; i < number; i++) {
-        number = numberButtons.innerText;
-        display++;
-    }
-    
-}
-
-// to activate operations when clicked
-function chooseOperation(operation) {
-
-}
-
-// to compute the input
-function compute() {
-
-}
-
-// to update display
-function updateDisplay() {
-    numberButtons.addEventListener('click' , e) => {
-        if (e.target.matches('[data-key')) {
-
-        }
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
     }
 }
+
+// To clear numbers when you press 'AC'
+clear() {
+    this.currentOperand = '';
+    this.previousOperand = '';
+    this.operation = undefined;
+}
+
+// To delete numbers when you press 'DEL'
+delete() {
+
+}
+
+// To add number to display when you click a number
+appendNumber(number) {
+    this.currentOperand = number;
+}
+
+// To activate operation when you click on any of the operations
+chooseOperation(operation) {
+
+}
+
+// To compute a single value when you put numbers together with an operation
+compute() {
+
+}
+
+// To update the display with the computed value
+updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand;
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const deleteButton = document.querySelector('[data-delete]');
+const clearButton = document.querySelector('[data-all-clear]');
+const equalsButton = document.querySelector('[data-equals]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => (
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    ))
+})
