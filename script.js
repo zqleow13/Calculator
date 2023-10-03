58fe9ca5ef14a380d33fd514ee30e8853e114f50
zqleow13 <hayleyleow13@gmail.com>
2023-10-03 17:50:14 +0800

    Added in event for compute and display operation

--- script.js
+++ script.js
@@ -20,13 +20,21 @@ class Calculator {
 
     // To add number to display when you click a number
     // Reason why I changed currentOperand and number to string is because I want JS to not add both as integers but rather just append both.
+    // Also, made sure that the period can only display once
     appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
     // To activate operation when you click on any of the operations
     chooseOperation(operation) {
-
+        if (this.currentOperand === '') return
+        if (this.previousOperand != '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
 
     // To compute a single value when you put numbers together with an operation
@@ -37,6 +45,7 @@ class Calculator {
     // To update the display with the computed value
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
     }
 }
 
@@ -52,7 +61,19 @@ const calculator = new Calculator(previousOperandTextElement, currentOperandText
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        calculator.appendNumber(button.innerText);
-        calculator.updateDisplay();
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
     })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
 })
