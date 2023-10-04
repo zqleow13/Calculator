f8da1f92d1900e087d776df3807a8296a87a8930
zqleow13 <hayleyleow13@gmail.com>
2023-10-04 15:41:14 +0800

    Added in commas for display
    As the user appends more numbers to the display, the commas help to show the distinction in place value.

--- script.js
+++ script.js
@@ -14,8 +14,10 @@ class Calculator {
     }
 
     // To delete numbers when you press 'DEL'
+    // Have to convert current operand to a string in order to delete number by number
+    // Have to slice off the last number to delete
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     // To add number to display when you click a number
@@ -64,11 +66,34 @@ class Calculator {
         this.operation = undefined
         this.previousOperand = ''
     }
+    // To get commas when you display large numbers
+    // Have to split the number to 'ones' and 'dp' so that the period can show up in display
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0 }) // To make sure there aren't any more decimal values after this gets converted into a string
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
 
     // To update the display with the computed value
+    // For previous operand, you want the operation to show alongside it when you add the operation
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
     }
 }
 
@@ -98,5 +123,15 @@ operationButtons.forEach(button => {
 
 equalsButton.addEventListener('click', button => {
     calculator.compute()
+    calculator.updateDisplay()
+})
+
+clearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
     calculator.updateDisplay()
 })
